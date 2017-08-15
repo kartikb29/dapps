0be92dd9ed382f5aa5084f40de53df7b1a24abce
kartikeyabhargava95 <kartikeyabhargava95@gmail.com>
2017-08-16 02:11:04 +0530

    KartikCoin added

---
+++ dapp2/contracts/KartikCoinContract.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.4;
+
+contract KartikCoinContract {
+
+  address owner;
+  event _txstatus(string status);
+  mapping (address => uint) public balances;
+
+  function KartikCoinContract() {
+    owner=msg.sender;
+    balances[owner]+=21000000;
+  }
+
+  modifier ifOwner(){
+    if (msg.sender!=owner) throw;
+    _;
+  }
+
+  function getOwner() constant returns(address) {
+    return owner;
+  }
+
+  function createCoins(uint _amt) ifOwner {
+    balances[owner]+= _amt;
+    _txstatus("Coins created!");
+  }
+
+  function getBalance(address _account) constant returns(uint) {
+    return balances[_account];
+  }
+
+  function txCoins(address _from, address _to, uint _amt) {
+    if ((balances[_from] >= _amt) && (_from == msg.sender)) {
+      balances[_from]-=_amt;
+      balances[_to]+= _amt;
+      _txstatus("Transfer successful");
+    }
+    _txstatus("Transaction unsuccessful");
+  }
+
+  function withdrawCoins(address _from, uint _amt) {
+    if ((balances[_from] >= _amt) && (_from == msg.sender)) {
+      balances[_from]-= _amt;
+      _txstatus("Withdrawal completed");
+    }
+  }
+}
--- dapp2/migrations/2_deploy_contracts.js
+++ dapp2/migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var KartikCoin = artifacts.require("./KartikCoinContract.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(KartikCoin);
 };
