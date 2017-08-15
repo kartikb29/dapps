e4f70481ed8c0430c8e7fbc866d5157d261c231e
kartikeyabhargava95 <kartikeyabhargava95@gmail.com>
2017-08-16 01:27:57 +0530

    deleted files

--- migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-var HelloWorld = artifacts.require("./HelloWorldContract.sol")
-var KartikCoin = artifacts.require("./KartikCoinContract.sol")
-module.exports = function(deployer) {
-  deployer.deploy(HelloWorld);
-  deployer.deploy(KartikCoin);
-};
